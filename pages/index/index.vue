85cb5298df65c6e30439d705755d9b27c377b5c0
ialmost <ialmost@qq.com>
2020-07-28 15:24:05 +0800

    添加中奖概率

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -46,7 +46,10 @@
         targetIndex: 0,
         // 中奖类目名称
         targetName: '',
-        canvasImg: ''
+        // 是否由前端控制概率
+        isFrontend: true,
+        // 权重随机数的最大值
+        weightTotal: 10000
       }
     },
     methods: {
@@ -66,29 +69,53 @@
           clearTimeout(stoTimer)
           stoTimer = null
           
+          // stock 奖品库存
+          // weight 中奖概率，数值越大中奖概率越高
+          // 如果存在谢谢参与的选项，请把权重值设置为权重随机数的最大值
           this.prizeList = [
-            { name: '0.1元现金奖', count: 10 },
-            { name: '10元现金奖', count: 0 },
-            { name: '5元话费', count: 1 },
-            { name: '50元现金奖', count: 0 },
-            { name: '1卷抽纸', count: 3 },
-            { name: '0.02元现金奖', count: 8 },
-            { name: '50金币', count: 4 },
-            { name: '100金币', count: 0 }
+            { prizeId: 1, name: '0.1元现金', stock: 10, weight: 10 },
+            { prizeId: 2, name: '10元现金', stock: 0, weight: 0 },
+            { prizeId: 3, name: '5元话费', stock: 1, weight: 5 },
+            { prizeId: 4, name: '50元现金', stock: 0, weight: 0 },
+            { prizeId: 5, name: '1卷抽纸', stock: 3, weight: 200 },
+            { prizeId: 6, name: '0.02元现金', stock: 8, weight: 900 },
+            { prizeId: 7, name: '谢谢参与', stock: 100, weight: 10000 },
+            { prizeId: 8, name: '100金币', stock: 0, weight: 500 }
           ]
         }, 500)
       },
       // 本次抽奖开始
       handleActionStart () {
+        let list = [...this.prizeList]
+        
         // 模拟请求
         let stoTimer = setTimeout(() => {
           clearTimeout(stoTimer)
           stoTimer = null
           
-          // 随机一个下标获奖
-          this.targetIndex = Math.floor(Math.random() * this.prizeList.length)
-          console.log('中奖商品 =>', this.prizeList[this.targetIndex].name)
-          console.log('商品数量 =>', this.prizeList[this.targetIndex].count)
+          // 判断是否由前端控制概率
+          // 前端控制概率的情况下，用最新的随机权重比对每一个奖项的权重，只有小于或等于才算中奖
+          // 后端控制概率的情况下，通常会直接返回奖品Id
+          if (this.isFrontend) {
+            let weight = Math.floor(Math.random() * this.weightTotal)
+            list.forEach((item, index) => {
+              if (weight <= item.weight) {
+                // 中奖下标
+                this.targetIndex = index
+              }
+            })
+          } else {
+            let prizeId = Math.floor(Math.random() * list.length)
+            list.forEach((item, index) => {
+              if (item.prizeId === prizeId) {
+                // 中奖下标
+                this.targetIndex = index
+              }
+            })
+          }
+          
+          console.log('抽中奖品 =>', this.prizeList[this.targetIndex].name)
+          console.log('奖品库存 =>', this.prizeList[this.targetIndex].stock)
           // 开始执行旋转定位
           this.$refs.raffleWheel.handleStartRotate(this.targetIndex)
         }, 200)
@@ -96,7 +123,9 @@
       // 本次抽奖结束
       handleActionEnd () {
         console.log('旋转结束，执行拿到结果后到逻辑')
-        this.targetName = '恭喜您，获得 ' + this.prizeList[this.targetIndex].name
+        let prizeName = this.prizeList[this.targetIndex].name
+        let prizeStock = this.prizeList[this.targetIndex].stock
+        this.targetName = prizeName === '谢谢参与' ? prizeName : prizeStock ? `恭喜您，获得 ${prizeName}` : '很抱歉，您来晚了，当前奖品已无库存'
       },
       // 抽奖转盘绘制完成
       handleDrawDone () {
@@ -128,7 +157,7 @@
   }
   .home-title__text {
     color: #000;
-    font-size: 40px;
+    font-size: 30px;
     font-weight: bold;
   }
   .home-result {
@@ -150,18 +179,17 @@
     position: absolute;
     left: 0;
     right: 0;
-    bottom: 60rpx;
+    bottom: 40rpx;
     width: 260rpx;
     height: 80rpx;
-    line-height: 80rpx;
-    border-radius: 10rpx;
-    text-align: center;
-    background-color: red;
-    margin: 0 auto;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
+    border-radius: 10rpx;
+    text-align: center;
+    background-color: red;
+    margin: 0 auto;
   }
   .home-action__text {
     color: #FFFFFF;
